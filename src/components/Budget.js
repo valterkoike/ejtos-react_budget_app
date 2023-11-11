d234677a5599426e155b3a97d133830b3d4319e9
Valter Koike <valterk@gmail.com>
2023-11-10 20:13:22 -0500

    Completed task 2: editable budget value with minimum and maximum

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,11 +4,22 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
     const handleBudgetChange = (event) => {
         let newBudget = event.target.value;
         if(parseInt(newBudget) > 20000) {
             document.getElementById('errMsgBudget').innerHTML = "The value cannot exceed 20,000";
             setNewBudget(20000);
+            return;
+        }
+        if(parseInt(newBudget) < totalExpenses) {
+            document.getElementById('errMsgBudget').innerHTML = "The value cannot be lower than the expenses (" + totalExpenses + ")";
+            setNewBudget(totalExpenses);
             return;
         }
         setNewBudget(newBudget);
