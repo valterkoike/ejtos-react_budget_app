585df78e7762a5cf08b56ac6b2f41fa3d5e9d336
Valter Koike <valterk@gmail.com>
2023-11-10 19:47:52 -0500

    Task 2-A. Added budget maximum of 20,000

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,12 +5,20 @@ const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        let newBudget = event.target.value;
+        if(parseInt(newBudget) > 20000) {
+            document.getElementById('errMsgBudget').innerHTML = "The value cannot exceed 20,000";
+            setNewBudget(20000);
+            return;
+        }
+        setNewBudget(newBudget);
+        document.getElementById('errMsgBudget').innerHTML = "";
     }
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<div id="errMsgBudget" style={{color:'red',paddingTop:'10px',fontWeight:'bold',display:'block',width:'100%',textAlign:'center'}}></div>
 </div>
     );
 };
